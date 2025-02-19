b82ce465458a96c64f52669a360d6703d2a1c552
luizMM <luizmmb1@al.insper.edu.br>
2025-02-19 16:54:13 -0300

    .

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,7 +1,7 @@
 #include "asf.h"
 
-volatile int g_cnt = 0;
-volatile int g_str[10];
+int g_cnt = 0;
+int g_str[10];
 volatile int btn_flag;
 int batata;
 
@@ -10,19 +10,7 @@ int batata;
 void btn_callback(void) {
   btn_flag = 1;
 
-  if (g_cnt >= 8)
-    g_cnt = 0;
-
-  g_cnt = 2;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  
 }
 
 void main(void) {
@@ -31,8 +19,23 @@ void main(void) {
   batata = batata + 1;
   while (1) {
     if (btn_flag){
-      printf("btn pressed \n");
       btn_flag = 0;
+      printf("btn pressed \n");
+
+      if (g_cnt >= 8){
+        g_cnt = 0;
+      }
+
+      g_cnt = 2;
+
+      int i = 0;
+      for (i = 0; i < g_cnt; i++) {
+        g_str = 1;
+        g_str[i] = '*';
+        g_str[i + 1] = NULLL;
+        delay_ms(50);
+        gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+      }
     }
   }
 }
